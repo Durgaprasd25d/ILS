ebd0e1a3071b862b79d3dfeecc344db4594632f0
Durgaprasd25d <durgaprasaddalai10@gmail.com>
2026-03-12 16:28:59 +0530

    added schema chnages

--- interiq-client/src/components/Schema.jsx
+++ interiq-client/src/components/Schema.jsx
@@ -5,59 +5,66 @@ import React from 'react';
  * Injects JSON-LD structured data into the <head>.
  */
 const Schema = ({ type, data }) => {
-  const schemas = {
-    LocalBusiness: {
-      "@context": "https://schema.org",
-      "@type": "LocalBusiness",
-      "name": "INTERIQ INTERIORS",
-      "image": "https://interiqinteriors.com/logo.png",
-      "@id": "https://interiqinteriors.com",
-      "url": "https://interiqinteriors.com",
-      "telephone": "+919437222340",
-      "address": {
-        "@type": "PostalAddress",
-        "streetAddress": "Janpath Road, Saheed Nagar",
-        "addressLocality": "Bhubaneswar",
-        "postalCode": "751007",
-        "addressRegion": "Odisha",
-        "addressCountry": "IN"
-      },
-      "geo": {
-        "@type": "GeoCoordinates",
-        "latitude": 20.2847,
-        "longitude": 85.8412
-      },
-      "openingHoursSpecification": {
-        "@type": "OpeningHoursSpecification",
-        "dayOfWeek": [
-          "Monday",
-          "Tuesday",
-          "Wednesday",
-          "Thursday",
-          "Friday",
-          "Saturday"
+  const buildSchema = () => {
+    if (type === "LocalBusiness") {
+      return {
+        "@context": "https://schema.org",
+        "@type": "LocalBusiness",
+        "name": "INTERIQ INTERIORS",
+        "image": "https://interiqinteriors.com/logo.png",
+        "@id": "https://interiqinteriors.com",
+        "url": "https://interiqinteriors.com",
+        "telephone": "+919437222340",
+        "address": {
+          "@type": "PostalAddress",
+          "streetAddress": "Janpath Road, Saheed Nagar",
+          "addressLocality": "Bhubaneswar",
+          "postalCode": "751007",
+          "addressRegion": "Odisha",
+          "addressCountry": "IN"
+        },
+        "geo": {
+          "@type": "GeoCoordinates",
+          "latitude": 20.2847,
+          "longitude": 85.8412
+        },
+        "openingHoursSpecification": {
+          "@type": "OpeningHoursSpecification",
+          "dayOfWeek": [
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday"
+          ],
+          "opens": "10:00",
+          "closes": "20:00"
+        },
+        "sameAs": [
+          "https://www.facebook.com/interiqinteriors",
+          "https://www.instagram.com/interiqinteriors",
+          "https://wa.me/919437222340"
         ],
-        "opens": "10:00",
-        "closes": "20:00"
-      },
-      "sameAs": [
-        "https://www.facebook.com/interiqinteriors",
-        "https://www.instagram.com/interiqinteriors",
-        "https://wa.me/919437222340"
-      ],
-      ...data
-    },
-    BreadcrumbList: {
-      "@context": "https://schema.org",
-      "@type": "BreadcrumbList",
-      "itemListElement": (data || []).map((item, index) => ({
-        "@type": "ListItem",
-        "position": index + 1,
-        "name": item.name,
-        "item": item.url
-      }))
-    },
-    BlogPosting: {
+        ...data
+      };
+    }
+
+    if (type === "BreadcrumbList") {
+      return {
+        "@context": "https://schema.org",
+        "@type": "BreadcrumbList",
+        "itemListElement": (Array.isArray(data) ? data : []).map((item, index) => ({
+          "@type": "ListItem",
+          "position": index + 1,
+          "name": item.name,
+          "item": item.url
+        }))
+      };
+    }
+
+    if (type === "BlogPosting") {
+      return {
         "@context": "https://schema.org",
         "@type": "BlogPosting",
         "mainEntityOfPage": {
@@ -80,10 +87,13 @@ const Schema = ({ type, data }) => {
           }
         },
         "datePublished": data?.date
-      }
+      };
+    }
+
+    return data;
   };
 
-  const schemaData = schemas[type] || data;
+  const schemaData = buildSchema();
 
   return (
     <script type="application/ld+json">
